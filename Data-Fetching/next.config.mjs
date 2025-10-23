1416566e03efa0976b038021b515ecdde14f00e7
Shivansh <abhinavnerd@gmail.com>
2025-10-24 00:14:35 +0530

    image component

--- Data-Fetching/next.config.mjs
+++ Data-Fetching/next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh4.googleusercontent.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
